9148d32efce21bbc2653fe756ca3640f0af8a993
PET <dioogoven@gmail.com>
2016-10-11 15:56:33 -0300

    Classes Person e Client prontas!

---
+++ src/model/Client.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Client extends Person {
+	private double amountSpent;
+	private double balance;
+	
+	public double getAmountSpent() {
+		return amountSpent;
+	}
+	public void setAmountSpent(double amountSpent) {
+		this.amountSpent = amountSpent;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
+}
--- src/model/Person.java
+++ src/model/Person.java
@@ -1,5 +1,36 @@
 package model;
 
 public class Person {
-
+	private int id;
+	private String name;
+	private String dateOfBirth;
+	private String phone;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
 }
