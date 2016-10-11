d2f5c49a54ad4f356f13122c00106e5f6b930bbc
PET <dioogoven@gmail.com>
2016-10-11 15:57:44 -0300

    Metodos de Category e Goods adicionados

--- src/model/Goods.java
+++ src/model/Goods.java
@@ -1,7 +1,32 @@
+package model;
+
 public class Goods {
-  private int code;
-  private float price;
-  private String name, description;
-
-
+	private int code;
+	private float price;
+	private String name, description;
+	
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
