babfe610c305e4f428df7feebfe8cdfe14db816f
Higor Anjos <higor.araujo.anjos@gmail.com>
2016-11-18 01:13:50 -0300

    SQL | Banco de dados atualizado

--- src/main/resources/sql/db_guaracai.sql
+++ src/main/resources/sql/db_guaracai.sql
@@ -193,7 +193,7 @@ CREATE TABLE tb_goods (
     code integer NOT NULL,
     price double precision NOT NULL,
     name character varying(50) NOT NULL,
-    description character varying(100),
+    description character varying(300),
     category_id integer NOT NULL,
     size character varying(30) NOT NULL
 );
@@ -306,7 +306,7 @@ ALTER TABLE ONLY tb_sale ALTER COLUMN id SET DEFAULT nextval('sale_id_seq'::regc
 -- Name: category_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('category_id_seq', 16, true);
+SELECT pg_catalog.setval('category_id_seq', 18, true);
 
 
 --
@@ -320,7 +320,7 @@ SELECT pg_catalog.setval('client_id_seq', 8, true);
 -- Name: employee_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('employee_id_seq', 4, true);
+SELECT pg_catalog.setval('employee_id_seq', 6, true);
 
 
 --
@@ -334,7 +334,7 @@ SELECT pg_catalog.setval('expenses_id_seq', 1, false);
 -- Name: produto_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('produto_id_seq', 2, true);
+SELECT pg_catalog.setval('produto_id_seq', 13, true);
 
 
 --
@@ -349,8 +349,6 @@ SELECT pg_catalog.setval('sale_id_seq', 1, false);
 --
 
 COPY tb_category (id, name, description) FROM stdin;
-1	Outros	Categoria sem definiçao ate o momento
-2	Verdes	Laranjas e Frutas
 3	Energéticos	Sucos com açai e guarana para melhorar sua energia
 4	Super energeticos	Aumentam ainda mais sua energia
 5	Funcionais	Deixam seu organismo em perfeito funcionamento
@@ -365,6 +363,7 @@ COPY tb_category (id, name, description) FROM stdin;
 14	Frozen	Casquinha ou frozen no copinho
 15	Montado	O cliente faz a sua escolha
 16	Extra	Qualquer condimento adicionado
+18	Outros	Produtos diversos como água de coco, água mineral etc.
 \.
 
 
@@ -375,9 +374,9 @@ COPY tb_category (id, name, description) FROM stdin;
 COPY tb_client (id, name, date_of_birth, phone, amount_spent, balance, email) FROM stdin;
 4	Higor Anjos	01/02/1002	(83) 10210-2111	0	0	higor.anjos@cc.ci.ufpb.br
 5	Marcos Henrique Alves	01/20/0121	(02) 11002-1012	0	0	marcosalves
-6	Higor Nobrega	12/21/2122	(12) 21222-122 	0	0	maorcos
-7	Marcos He	08/11/1194	(83) 89012-3292	12.4000000000000004	12.4000000000000004	alves.gg@hotmail.com
 8	Rubia Alves	12/03/2012	(93) 22991-2212	0	0	rubenita21@gmailc.om
+6	Igor Nobrega	12/21/2122	(12) 21222-122 	0	0	maorcos
+7	Matheus Praxedes	08/11/1194	(83) 89012-3292	12.4000000000000004	12.4000000000000004	xedes@hotmail.com
 \.
 
 
@@ -386,9 +385,9 @@ COPY tb_client (id, name, date_of_birth, phone, amount_spent, balance, email) FR
 --
 
 COPY tb_employee (id, name, date_of_birth, phone, email, cpf, username, password, type, agency, count, complement, number, city, street, district, state) FROM stdin;
-1	Marcos Henrique Alves da Silva	08111995  	83999606821    	marcos.alves@cc.ci.ufpb.br	10000974471   	alvesmarcos	quinho	Administrador	\N	\N	Apto 401	611	João Pessoa	Rua Rejane Freire Matos	Bancários	PB
-3	marcos	02/12/2012	(32) 3332-2333 	amdm	100.093.299-32	marcos	ey	Gerente	2332	msa		403	JP	JP	cidade unviersid	AC
 4	admin	25/32/1896	(83) 9918-7066 	higor.araujo@cc.ci.ufpb.br	529.655.972-04	admin	root	Gerente	2386432	356132	apt203	932	JP	JP	Bancarios	PB
+5	marcos	07/11/1995	(83) 9992-1534 	marcos.alves@cc.ci.ufpb.br	215.321.452-00	marcos	senha	Caixa	15448	35484	Apt 401	234	João Pessoa	João Pessoa	Bancários	PB
+6	diogo	07/09/1996	(83) 9988-7123 	dioogoven@gmail.com	117.542.331-01	diogo	senha	Caixa	45486	21564	Apt 302	424	João Pessoa	João Pessoa	Bancarios	PB
 \.
 
 
@@ -405,9 +404,18 @@ COPY tb_expenses (id, description, cost, employee_id) FROM stdin;
 --
 
 COPY tb_goods (id, code, price, name, description, category_id, size) FROM stdin;
-0	12	12	Maça	Um fruta	1	unico
-1	190	7.20000000000000018	Leite de manga	Leite de manga vindo da vaca.	1	Unico
 2	90	0.5	Amendoim extra	porção extra de amendoim	16	Porção
+3	1	5	Guaracai	acai + guarana em po + xarope de guarana	3	300ml
+4	1	6	Guaracai	Acai + guarana em po + xarope de guarana	3	500ml
+5	6	6	Guaratudo-doim	guarana em po + xarope de guarana + catuaba + marapuama + ginseng + amendoim + castanha + leite + gelo + limao	4	300ml
+6	6	7	Guaratudo-doim	guarana em po + xarope de guarana + catuaba + marapuama + ginseng + amendoim + castanha + leite + gelo + limao	4	500ml
+7	8	5	Acerola + Tangerina	Suco de Acerola com Tangerina	5	300ml
+8	8	6	Acerola + Tangerina	Suco de Acerola com Tangerina	5	500ml
+9	17	6	Acerola + Caju + Limao	Suco de Acerola com Caju e Limao	6	300ml
+10	17	7	Acerola + Caju + Limao	Suco de Acerola com Caju e Limao	6	500ml
+11	100	10	Açaí na Tigela	Açaí na tigela com 3 ingredientes	13	300ml
+12	100	12	Açaí na Tigela	Açaí na Tigela com 3 ingredientes	13	500ml
+13	76	2.5	Água mineral	Água mineral gelada	18	510ml
 \.
 
 
